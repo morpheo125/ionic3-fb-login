c87a148b0de4baf5480e790ec9c361bab29292a4
David <morpheo125@gmail.com>
2017-05-26 14:35:14 +0200

    Added: - google login - Email login - Create user with email - Update profile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+// Do not import from 'firebase' as you'd lose the tree shaking benefits
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user: Observable<firebase.User>;
+  showUpdateProfile: boolean = false;
+  profile: any = {
+    first: '',
+    last: '',
+    photoURL: ''
+  };
 
+  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  loginRedirect() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  loginWithEmail(email, password) {
+    console.log('email - ', email);
+    console.log('password - ', password);
+    this.afAuth.auth.signInWithEmailAndPassword(email, password).catch(
+      error => {
+        alert(error);
+      }
+    );
+  }
+
+  createUserWithEmail(email, password) {
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password).catch(
+      error => {
+        alert(error);
+      }
+    );
+  }
+
+  updateProfile() {
+    const user = this.afAuth.auth.currentUser;
+    const profile = {
+      displayName: (this.profile.first + ' ' + this.profile.last) || user.displayName,
+      photoURL: this.profile.photoURL || user.photoURL
+    };
+
+    user.updateProfile(profile).catch(error => {
+      console.error('error - ', error);
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
