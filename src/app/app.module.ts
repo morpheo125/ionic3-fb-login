93a07f7ca662f912e541ba27812eebef9ffa3a0e
David <morpheo125@gmail.com>
2017-05-26 11:46:12 +0200

    Add angularFire config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+// third party libs
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// plugins
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// providers
+
+// pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { environment } from '../environment/environment';
+
+let pages = [
+  MyApp,
+  HomePage,
+];
+
+export function declarations() {
+  return pages;
+}
+
+export function entryComponents() {
+  return pages;
+}
+
+export function providers() {
+  return [
+    SplashScreen,
+    StatusBar,
+    // Keep this to enable Ionic's runtime error handling during development
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ];
+}
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: declarations(),
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my-app'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: entryComponents(),
+  providers: providers()
 })
 export class AppModule {}
